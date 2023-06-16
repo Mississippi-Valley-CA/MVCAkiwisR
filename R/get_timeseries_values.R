b326abe493824a1e7541419fa9e36a7931181c47
Daniel Post <dpost@mvc.on.ca>
2023-06-16 13:59:52 -0400

    ts list now read from live quinte kiWIS

--- R/get_timeseries_values.R
+++ R/get_timeseries_values.R
@@ -11,38 +11,38 @@
 
 get_timeseries_values <- function() {
     ## Load the CSV file #
-    input <- read.csv(file=file.path("data", "mvca_wiski_timeseries.csv"), header=TRUE, sep = ",", dec = ".")
+    input <- read.csv("https://waterdata.quinteconservation.ca/KiWIS/KiWIS?format=csv&datasource=0&timezone=EST&dateformat=yyyy-MM-dd%20HH:mm:ss&service=kisters&type=queryServices&metadata=false&request=getTimeseriesList&station_no=MVCA1*,MVCA2*,MVCA3*,MVCA4*,MVCA5*,MVCA6*,MVCA7*,MVCA8*,MVCA9*,WISKI-0321,02KF01*,02KF001,02KF005,02KF006,02KF020&returnfields=station_name,station_no,ts_id,ts_name,parametertype_name,stationparameter_name,coverage", header=TRUE, sep = ";", dec = ".")
 
     ## Select the parameter #
-    parameters <- sort(unique(input$Parameter))
+    parameters <- sort(unique(input$parametertype_name))
     parameter <- select.list(parameters, title = "Select a Parameter")
 
     ## Narrow down the timeseries list by filtering for the parameter #
-    inputAfterParameter <- input[input$Parameter == parameter, ]
+    inputAfterParameter <- input[input$parametertype_name == parameter, ]
 
     # Select the station #
-    stations <- sort(unique(inputAfterParameter$Station))
+    stations <- sort(unique(inputAfterParameter$station_name))
     station <- select.list(stations, title = "Select a Station")
 
     ## Narrow down the timeseries list further by filtering for the station #
-    inputAfterStation <- inputAfterParameter[inputAfterParameter$Station == station, ]
+    inputAfterStation <- inputAfterParameter[inputAfterParameter$station_name == station, ]
 
     ## Select the period #
-    periods <- sort(unique(inputAfterStation$TS.Name))
+    periods <- sort(unique(inputAfterStation$ts_name))
     period <- select.list(periods, title = "Select a Period")
 
     ## Get the ts details and call in a variable #
-    timeseries <- inputAfterStation[inputAfterStation$TS.Name == period, ]
-    tsID <- timeseries$TS.ID
+    timeseries <- inputAfterStation[inputAfterStation$ts_name == period, ]
+    tsID <- timeseries$ts_id
 
     ## Pick a start date and an end date within the bounds available #
-    startDate <- as.Date(readline(prompt=paste0("Enter Start Date of ", substr(timeseries$From, 1, 10), " or later: ")),"%Y-%m-%d", tz = "EST")
+    startDate <- as.Date(readline(prompt=paste0("Enter Start Date of ", substr(timeseries$from, 1, 10), " or later: ")),"%Y-%m-%d", tz = "EST")
 
-    endDate <- as.Date(readline(prompt=paste0("Enter End Date of ", substr(timeseries$To, 1, 10), " or later: ")),"%Y-%m-%d", tz = "EST")
+    endDate <- as.Date(readline(prompt=paste0("Enter End Date of ", substr(timeseries$to, 1, 10), " or later: ")),"%Y-%m-%d", tz = "EST")
 
     ## Get the timeseries values #
     values <- ki_timeseries_values(
-    ts_id = timeseries$TS.ID,
+    ts_id = timeseries$ts_id,
     start_date = startDate,
     end_date = endDate
     )
--- devscript.R
+++ devscript.R
@@ -1,6 +1,6 @@
 ## Document ####################################################################
 devtools::document()
-
+usethis::use_tidy_description()
 ## Clean up ####################################################################
 
 ## Clear environment
@@ -11,46 +11,6 @@ dev.off()  # Only if there IS a plot
 
 ## Clear console
 cat("\014")  # ctrl+L
-
-## Get the timeseries ID #######################################################
-
-## Load the CSV file #
-input <- read.csv(file=file.path("data", "mvca_wiski_timeseries.csv"), header=TRUE, sep = ",", dec = ".")
-
-## Select the parameter #
-parameters <- sort(unique(input$Parameter))
-parameter <- select.list(parameters, title = "Select a Parameter")
-
-## Narrow down the timeseries list by filtering for the parameter #
-inputAfterParameter <- input[input$Parameter == parameter, ]
-
-# Select the station #
-stations <- sort(unique(inputAfterParameter$Station))
-station <- select.list(stations, title = "Select a Station")
-
-## Narrow down the timeseries list further by filtering for the station #
-inputAfterStation <- inputAfterParameter[inputAfterParameter$Station == station, ]
-
-## Select the period #
-periods <- sort(unique(inputAfterStation$TS.Name))
-period <- select.list(periods, title = "Select a Period")
-
-## Get the ts details and call in a variable #
-timeseries <- inputAfterStation[inputAfterStation$TS.Name == period, ]
-tsID <- timeseries$TS.ID
-
-## Pick a start date and an end date within the bounds available #
-prompt=paste0("No. of levels in attribute ", i, ":")
-startDate <- as.Date(readline(prompt=paste0("Enter Start Date of ", substr(timeseries$From, 1, 10), " or later: ")),"%Y-%m-%d", tz = "EST")
-
-endDate <- as.Date(readline(prompt=paste0("Enter End Date of ", substr(timeseries$To, 1, 10), " or later: ")),"%Y-%m-%d", tz = "EST")
-
-## Load the timeseries data #
-values <- ki_timeseries_values(
-  ts_id = timeseries$TS.ID,
-  start_date = startDate,
-  end_date = endDate
-)
 
 ## Testing #####################################################################
 values <- get_timeseries_values()
